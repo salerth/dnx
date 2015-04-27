3e9e685c8584b145b6558494157e3752539bfb34
Troy Dai <troy.dai@outlook.com>
2015-04-26 17:19:08 -0700

    Initial draft of JavaScriptSerializer

---
+++ src/Microsoft.Framework.Runtime/JsonDeserializer/JavaScriptSerializer.cs
@@ -0,0 +1,72 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="JavaScriptSerializer.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace System.Web.Script.Serialization
+{
+    using System;
+
+    public class JavaScriptSerializer
+    {
+        internal const int DefaultRecursionLimit = 100;
+        internal const int DefaultMaxJsonLength = 2097152;
+
+        private int _recursionLimit;
+        private int _maxJsonLength;
+
+        public JavaScriptSerializer()
+        {
+            RecursionLimit = DefaultRecursionLimit;
+            MaxJsonLength = DefaultMaxJsonLength;
+        }
+
+        public int MaxJsonLength
+        {
+            get
+            {
+                return _maxJsonLength;
+            }
+            set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentOutOfRangeException(AtlasWeb.JSON_InvalidMaxJsonLength);
+                }
+                _maxJsonLength = value;
+            }
+        }
+
+        public int RecursionLimit
+        {
+            get
+            {
+                return _recursionLimit;
+            }
+            set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentOutOfRangeException(AtlasWeb.JSON_InvalidRecursionLimit);
+                }
+                _recursionLimit = value;
+            }
+        }
+
+        public object DeserializeObject(string input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException("input");
+            }
+            if (input.Length > MaxJsonLength)
+            {
+                throw new ArgumentException(AtlasWeb.JSON_MaxJsonLengthExceeded, "input");
+            }
+
+            object o = JavaScriptObjectDeserializer.BasicDeserialize(input, RecursionLimit, this);
+            return o;
+        }
+    }
+}
